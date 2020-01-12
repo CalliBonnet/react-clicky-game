8f71aea8c110650290bfa525f7334346821b9f1c
Calli Bonnet <calli@paradox.ai>
2020-01-11 19:47:16 -0700

    added to the main container

---
+++ clicky-game-homework/src/components/GameContainer/GameContainer.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'; 
+import MemoryCards from '../MemoryCards/MemoryCards'; 
+import "./GameContanier.css"; 
+
+class GameContainer extends Component {
+    state = { 
+        images, 
+        message: "Click one of the images to begin!", 
+        score: 0, 
+        topScore: 0
+    }; 
+
+    handleClick = (id, clicked) => {
+        
+        const orderOfImages = this.state.images; 
+
+        if (clicked) {
+            orderOfImages.forEach((image, index) => {
+                orderOfImages[Index].clicked = false; 
+            });
+            return this.setState({
+                image: orderOfImages.sort(() => Math.random() - 0.5), 
+                message: "You Already Clicked this One", 
+                score: 0 
+            })
+        } else {
+            orderOfImages.forEach((image, index) => {
+                if (id === image.id) {
+                    orderOfImages[index].clicked = true; 
+                }
+            }); 
+
+            const { topScore, score } = this.state; 
+            const newScore = score + 1; 
+            const newTopScore = newScore > topScore ? newScore : topScore; 
+
+            return this.setState({
+                image: orderOfImages.sort(() => Math.random() -0.5), 
+                message: "Congrats!", 
+                score: newScore, 
+                topScore: newTopScore
+            })
+        }
+    }; 
+
+    render() {
+        return (
+            <div className="container-fuild mainCardCon">
+                <div className="gameMessage text-center">
+                    <p>{this.state.message}</p>
+                </div>
+                <div className="gameScores text-center">
+                    <p>Score: {this.state.score} | Top Score: {this.state.topScore}</p>
+                    </div> 
+                    <div className="container">
+                        <div className="row">
+                            {this.state.images.map(image => (
+                                <MemoryCards 
+                                key={image.id}
+                                id={image.id}
+                                name={image.name}
+                                clicked={image.clicked}
+                                image={image.image}
+                                handleClick={this.handleClick}
+                                />
+
+                            ))}
+                        </div>
+                    </div>
+            </div>
+        ); 
+    }; 
+}
+
+export default GameContainer; 
---
+++ clicky-game-homework/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import "./Header.css"; 
+
+const Header = () => {
+    <div className="Jumbotron Jumbotron-fuild">
+        <div className="container text-center">
+            <h1>Clicky Game!</h1>
+            <p>Click on any image to earn points... Just don't click on the same image twice!</p>
+        </div>
+    </div>
+}; 
+
+export default Header; 
